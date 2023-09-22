72ccffa7e492acafd9d230cfa6511a72f8a95579
Mattiasslzr <mattias2317@icloud.com>
2023-09-22 19:13:13 -0300

    Update UniversidadGrupo81.java
    d

--- src/universidadgrupo81/UniversidadGrupo81.java
+++ src/universidadgrupo81/UniversidadGrupo81.java
@@ -15,11 +15,11 @@ public class UniversidadGrupo81 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Connection con = Conexion.getConexion();
     }
     
 }
 
 
 
-/*  */
+/*           */ 
