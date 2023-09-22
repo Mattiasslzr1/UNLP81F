3ed4a7eba71fac6a24458b6b0f684f911e8978a1
Mattiasslzr <mattias2317@icloud.com>
2023-09-22 19:11:41 -0300

    Update Conexion.java

--- src/universisdadgrupo81/AccesoADatos/Conexion.java
+++ src/universisdadgrupo81/AccesoADatos/Conexion.java
@@ -1,21 +1,17 @@
-
 package universisdadgrupo81.AccesoADatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 
+
 public class Conexion {
-    private static final String URL="jdbc:mysql://localhost";
-    private static final String DB="universidadulp"
-    private static final String Usuario="root";
-    private static String PASSWORD="";        
-    
-    
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidadulp"
+    private static final String USUARIO="root";
+    private static final String PASSWORD = "";             
     private  static  Connection connection;
     
     //Metodo Constructor 
@@ -25,19 +21,16 @@ public class Conexion {
    public static Connection getConexion(){
        if (connection == null) {
            try { 
-               Class.forName("org.mariadb.jdbc.Driver");
-               // Setup the connection with the Db
+               Class.forName("org.mariadb.jdbc.Driver");             
                connection = DriverManager
-                       .getConnection (URL+DB + "?useLegacyDatetimeCode=flase&serverTimezone=UTC"
-                                        + "&user="+ USUARIO + "password=" + ¨PASSWORD);
+                       .getConnection (URL+DB ,USUARIO , PASSWORD );
                                         
-                                        
+           } catch(ClassNotFoundException ex){               
+               JOptionPane.showMessageDialog(null, "Error al cargar los Drivers ");
+                                         
            } catch (SQLException ex) {
-               JOptionPane.showMessageDialog(null, "Error al conectarse a la BD "+ex.getMessage());
-           }catch(ClassNotFoundException ex){
-               
-               JOptionPane.showMessageDialog(null,"Error al cargar los Drivers "+ex.getMesagge());
-               
+               JOptionPane.showMessageDialog(null, "Error al conectarse a la BD ") ;
+          
            }
        }
        
